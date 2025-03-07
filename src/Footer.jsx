e71ed50f3602332a9097e7cc5eeeb7b151ed807f
Niharika237 <battuniharika3@gmail.com>
2025-03-07 10:18:33 +0530

    c

---
+++ src/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Products()
+{
+    return (
+        <div>
+            this is footer
+        </div>
+    )
+}
---
+++ src/components/Products.jsx
@@ -0,0 +1,8 @@
+export default function()
+{
+    return (
+        <div>
+            This is product 1
+        </div>
+    )
+}
